0daa63da8e0aa238f0c07ee02afa17d444aee884
jclevine <jclevine.receipts@gmail.com>
2016-02-28 10:52:05 -0500

    Bad file remove script.

---
+++ playlists/bad_today.sh
@@ -0,0 +1,13 @@
+!#/bin/sh
+
+rm -f "/run/user/1000/gvfs/mtp:host.../music/Enon/Believo!/11 Biofeedback.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/Trashcan Sinatras/Fez/18 In Capitals.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/Compilations/Back to Mono (1958-1969)/4-13 Phil Spector and Artists - Silent Night.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/Wilco/Yankee Hotel Foxtrot/2-05 Reservations.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/The Rapture/Echoes/03 Open Up Your Heart.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/Compilations/Insound and Griffin Technology Present_ Pushing Product 8 Fall '06/02 Tokyo Police Club - Shoulders & Arms.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/Information Society/Information Society/02 Tomorrow.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/Compilations/The 2 Tone Collection_ A Checkered Past/1-05 The Specials - A Message to You Rudy (feat. Rico).mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/The Beach Boys/Pet Sounds/02 You Still Believe in Me.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/Seabound/No Sleep Demon v2.0/01 Smoke.mp3"
+rm -f "/run/user/1000/gvfs/mtp:host.../music/AC_DC/Highway to Hell/08 If You Want Blood (You've Got It).mp3"
