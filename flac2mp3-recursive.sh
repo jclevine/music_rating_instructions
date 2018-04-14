edb01dd9c845d0b4651424a8dc22900c2fd267a9
Justin Levine <justin.c.levine@gmail.com>
2018-04-14 17:54:36 -0400

    Removing stuff that's no longer relevant

--- flac2mp3-recursive.sh
+++
@@ -1,6 +0,0 @@
-find . -type d -links 2 -print0 | while read -d $'\0' a; do
-    pushd "$a"
-    parallel -i -j$(nproc) ffmpeg -y -i {} -qscale:a 0 "{}.mp3" < /dev/null -- ./*.flac
-    rename 's/.flac.mp3/.mp3/' ./*.mp3
-    popd
-done
